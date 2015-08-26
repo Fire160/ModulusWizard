522a7753f2f881e1e733d14a5c6d9f6b942dfd11
Michael Young <michael.young@MU-C1-08.mullen.pvt.k12.co.us>
2015-08-26 09:44:19 -0600

    Intial commit

---
+++ ModulusWizard/src/ModQuiz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ModQuiz
+	{
+	public static void main(String[] args)
+		{
+		Scanner things = new Scanner(System.in);
+		System.out.println("Hello what is your name?");
+		String name = things.nextLine();
+		int 
+		things.close();
+		}
+	}
